dc12370ae8a5e10376fffeec401b5554b544e51a
Eralde <eralde.g@gmail.com>
2021-11-13 18:07:11 +0300

    (chore) build-css.js: convert every LESS file from EXTRA_STYLE_FILES list into a CSS file

--- build-css.js
+++ build-css.js
@@ -10,6 +10,10 @@ const THEME_CSS_FILE_PREFIXES = [
     'theme-3.x',
 ];
 
+const EXTRA_STYLE_FILES = [
+    {input: path.join('extra', '3.7.40'), output: 'extra__3_7_40'},
+];
+
 const UI_EXTENSIONS_CSS_FILE_PREFIXES = [
     'uiExtensions-2.x',
     'uiExtensions-3.x',
@@ -29,11 +33,16 @@ const generateAllUiExtensions = async () => Promise.all(UI_EXTENSIONS_CSS_FILE_P
 const generateAllHideUiExtensions = async () => Promise.all(HIDE_UI_EXTENSIONS_CSS_FILE_PREFIXES.map(generateCss));
 const generateLegacyCss = async () => await exec(`npx lessc --glob ${THEME_DIR}/version-specific/${LEGACY_CSS_FILE_PREFIX}.less ${CSS_OUTPUT_DIR}/${LEGACY_CSS_FILE_PREFIX}.css`);
 
+const generateAllExtras = async () => Promise.all(EXTRA_STYLE_FILES.map((item) => {
+    return exec(`npx lessc --glob ${THEME_DIR}/${item.input}.less ${CSS_OUTPUT_DIR}/${item.output}.css`);
+}));
+
 const build = () => {
     return generateAllThemes()
         .then(generateAllUiExtensions)
         .then(generateAllHideUiExtensions)
         .then(generateLegacyCss)
+        .then(generateAllExtras)
         .then(async () => {
             await exec(`npx lessc ${THEME_DIR}/extensionPopup.less ${CSS_OUTPUT_DIR}/popup.css`);
             await exec(`npx lessc ${THEME_DIR}/extensionOptions.less ${CSS_OUTPUT_DIR}/options.css`);
